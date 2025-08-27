dbe1d20544d3ebf559603a1c6dea943be3db6fda
Your Name <you@example.com>
2025-08-27 10:27:46 +0530

    added header section.

---
+++ src/components/Menutem.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+ export default function  Menuitem ({title, address, Icon}) {
+  // const {title} = props
+  return (
+    <Link href={address} className='hover:text-amber-500'>
+      <Icon className="text-2xl sm:idden"/>
+      <p className='uppercase hidden sm:inline text-sm'>{title}</p>
+    </Link>
+  )
+ }
